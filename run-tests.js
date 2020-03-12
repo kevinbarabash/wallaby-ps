88f19f481011cb1b674690358d309e2ee1a0bf4b
Kevin Barabash <kevinb@khanacademy.org>
2020-03-12 00:05:49 -0400

    add wallaby config with pre-processor for PureScript and add unit tests

---
+++ run-tests.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const {main} = require("./output/Test.Main/index.js");
+
+main();
---
+++ wallaby.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = () => {
+    return {
+        files: [
+            "src/**/*.purs",
+            ".spago/*/*/src/**/*.purs", // avoid catenable-lists/v5.0.1/benchmarks/src/
+        ],
+
+        tests: [
+            "test/**/*.purs",
+        ],
+
+        postprocessor: function (wallaby) {
+            const promises = wallaby.allFiles.map(file => {
+                return file.getContent().then(content => {
+                    const match = content.match(/^module ([a-zA-Z\.]+)/m);
+                    const name = match[1];
+                    if (!name) {
+                        throw new Error("module name not found");
+                    }
+                    return name;
+                }).then(name => {
+                    const indexPath = path.join(__dirname, "output", name, "index.js");
+                    if (!fs.existsSync(indexPath)) {
+                        throw new Error(`no index.js found for ${name}: ${file.path}`);
+                    }
+
+                    const content = fs.readFileSync(path.join(__dirname, "output", name, "index.js"), "utf-8");
+                    const sourceMap = fs.readFileSync(path.join(__dirname, "output", name, "index.js.map"), "utf-8");
+
+                    return wallaby.createFile({
+                        path: file.path,
+                        original: file,
+                        content,
+                        sourceMap,
+                    });
+                });
+            });
+
+            return Promise.all(promises);
+        },
+    };
+}
